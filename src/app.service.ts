e366dd886766dc9d9477879f7c28c3817ae3ac3d
Muhammet Karadağ <Mk.MuhammetKaradag@gmail.com>
2021-12-14 11:16:45 +0300

    init project

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getsomething(): string {
+    return 'Something  can be   sent back ';
+  }
+}
